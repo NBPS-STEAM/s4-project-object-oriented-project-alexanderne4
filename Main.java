4c818fe9b80fa84ec8e7bbcfc461066173546ca6
Alexander Nechaev <alexander.nechaev@mynbps.org>
2021-01-31 22:56:44 +0000

    Added a playerSetUp class to the App

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+class App {
+
+  String playerName; 
+  int choice; 
+  int gun;
+  int medkit;
+  int flashlight;
+  int map;
+  int password; 
+  int shadowHp = 100; 
+  int playerHp = 100;
+  double radiation; 
+  int line;
+
+      public static void main(String[] args) { 
+        player newPlayer = new player();
+        newPlayer.playerSetUp();
+      } 
+  
+  }
---
+++ player.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class player {
+  Scanner myScanner = new Scanner(System.in);
+  String playerName;
+  int playerHp = 100;
+  public void playerSetUp(){
+    System.out.println("-----------------------------------------------------------------");
+    System.out.println("                       12:08AM                             ");
+   
+   int timeToWait = 10; 
+        System.out.println("[CONNECTING...]");
+        try {
+            for (int i=0; i<timeToWait ; i++) {
+                Thread.sleep(500);
+                System.out.println("_._._._._");
+            }
+        } catch (InterruptedException ie)
+        {
+            Thread.currentThread().interrupt();
+        }
+   System.out.println("[AUTHORIZING...]");
+   System.out.println("[SUCCESSFUL...]");
+   System.out.println("Hey there, can anyone see it?");
+
+
+		System.out.println("My name is Coueus, I'm the crew member of the secret operation");
+    System.out.println("Well, last crew member....");
+    System.out.println("What is your name stranger?");
+      playerName = myScanner.nextLine();
+      System.out.println("Hmmm... " + playerName + ", sounds promising..");
+  }
+}
